2284905a46055bd0a46023dfd95de648c368df05
Lori Culberson <bigskybandit@sbcglobal.net>
2021-02-06 15:06:15 -0500

    Greeting displays based on time of day

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,5 +1,16 @@
 function Welcome(){
-  return <h2>Welcome Astros Fans!</h2>
+  const currentHour = new Date().getHours()
+  const message = currentHour < 5 ? "Why are you still up?" 
+                : currentHour < 12 ? "Good Morning"
+                : currentHour < 18 ? "Good Afternoon"
+                : "Good Evening"
+
+  return (
+    <>
+      <p>{message} sports fans!</p>
+      <h2>Welcome Astros Fans!</h2>
+    </>
+  )
 }
 
 export default Welcome;
